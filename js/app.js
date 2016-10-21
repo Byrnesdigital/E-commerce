53bb0cde324e53d28ead96b6303923948b3923aa
= <=>
2016-10-20 21:04:19 -0500

    Update login spotify

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const redirect_uri = "localhost";
+const redirect_uri = "https://young-reef-87125.herokuapp.com/index.html";
 const client_secret = "";
 const client_id = "49c21ca8c1754ea7800c50be00dae758";
 
@@ -18,41 +18,27 @@ var searchAlbums = function(query) {
     });
 };
 
-var authenticationAPI = function(){
-  var scopes = 'user-read-private user-read-email';
-  $.ajax({
-      method: "GET",
-      url: 'https://accounts.spotify.com/authorize',
-      data: {
-          response_type: 'code',
-          client_id: client_id,
-          scope:encodeURIComponent(scopes),
-          redirect_uri:encodeURIComponent(redirect_uri)
-
-      },
-      success: function (response) {
-          console.log(response);
-      }
-  });
-  /* var url = 'https://accounts.spotify.com/authorize' +
-  '?response_type=code' +
-  '&client_id=' + client_id +
-  (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-  '&redirect_uri=' + encodeURIComponent(redirect_uri);
-
-  jQuery.ajax( {
-    url: 'https://api.spotify.com/v1/users/fabiing10/playlists/02SelNGqy8Op0dqp1iXZtz/tracks',
-    type: 'POST',
-    data: { content: 'testing test' },
-    beforeSend : function( xhr ) {
-        xhr.setRequestHeader( "Authorization", "BEARER " + access_token );
-    },
-    success: function( response ) {
-        // response
+function generateRandomString(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
-  } );*/
+    return text;
+  };
 
-}
+var authenticationAPI = function(){
+  var state = generateRandomString(10);
+  var scope = 'user-read-private user-read-email';
+  var url = 'https://accounts.spotify.com/authorize';
+  url += '?response_type=token';
+  url += '&client_id=' + encodeURIComponent(client_id);
+  url += '&scope=' + encodeURIComponent(scope);
+  url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+  url += '&state=' + encodeURIComponent(state);
+  window.location = url;
+
+};
 
 
 document.addEventListener("DOMContentLoaded",function(event){//Una vez la página ha cargado.
