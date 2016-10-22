d04c10db3f13058fd0396782e6a06858105ecdd8
Carolina Nino <cnino@iconoi.com>
2016-10-21 21:17:48 -0500

    Valoración estrellas y reproducir

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded",function(event){//Una vez la págin
   list_gallery = Array.from(list_gallery);//vuelve una lista en arreglo
   var aplha_order_button = document.querySelector("#alph-order-button");//obtiene el botón de ordenar alfabeticamente
   aplha_order_button.addEventListener("click", function(){//evento del botón alfabetico
-    document.querySelector("#artist-gallery").innerHTML = "";
+    document.querySelector("#artist-gallery").innerHTML = "";//Cambia la sintaxis HTML describiendo los descendientes del elemento
     list_gallery.sort(function(a,b){
       if(a.querySelector(".artist-mini-name").textContent < b.querySelector(".artist-mini-name").textContent){
         return -1;
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded",function(event){//Una vez la págin
       }
     });
     list_gallery.forEach(function(element ,index ,array){
-      document.querySelector("#artist-gallery").appendChild(element);
+      document.querySelector("#artist-gallery").appendChild(element);//Adiciona un nodo como último hijo de un nodo
     });
   });
   var number_order_button = document.querySelector("#num-order-button");//obtiene el botón de ordenar numericamente
@@ -31,6 +31,14 @@ document.addEventListener("DOMContentLoaded",function(event){//Una vez la págin
     });
     list_gallery.forEach(function(element ,index ,array){
       document.querySelector("#artist-gallery").appendChild(element);
+    });
+  });
+  var play = document.querySelectorAll(".reproducir");
+  play = Array.from(play);
+  console.log("play:  "+play);
+  play.forEach(function(element_li, index, array){
+    element_li.addEventListener("click", function(){
+      var repplay = document.getElementById("reproducir").play();
     });
   });
 });
